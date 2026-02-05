26eab5ebb80ca4802f6105db025d3d0c4f9c83f2
Baxtiyor Abduxamidov <baxtiyorabduxamidov32@gmail.com>
2026-02-05 16:18:12 +0500

    Create codes-23-random.py

---
+++ 23-dars-Modullar/codes-23-random.py
@@ -0,0 +1,18 @@
+import random as r # random modulini r deb chaqirayapmiz
+
+# son = r.randint(0,100) # 0 va 100 oralig'ida tasodifiy son
+# print(son)
+
+# ismlar = ['olim','anvar','hasan','husan']
+# ism = r.choice(ismlar) # ismlar dan tasodifiy ism tanlaymiz
+# print(ism)
+# print(r.choice(ism)) # ismdan tasodifiy harf tanlaymiz
+
+# x = list(range(0,51,5))
+# print(x)
+# print(r.choice(x))
+
+# x = list(range(11))
+# print(x)
+# r.shuffle(x)
+# print(x)
